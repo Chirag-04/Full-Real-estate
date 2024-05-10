b2d8c94e5a769ea07b2cf8c75d796a248e1672a3
Chirag-04 <95089015+Chirag-04@users.noreply.github.com>
2024-05-10 12:45:10 +0530

    created the sign up page

--- api/app.js
+++ api/app.js
@@ -1,4 +1,5 @@
 import express from "express"
+import cors from "cors"
 import postRoute from './routes/post.route.js'
 import authRoute from './routes/auth.route.js'
 import cookieParser from "cookie-parser";
@@ -6,8 +7,10 @@ import cookieParser from "cookie-parser";
 const app =  express();
 
 // to allow out app to send json objects 
+app.use(cors({origin:'http://localhost:5173', credentials: true}))
 app.use(express.json());
 app.use(cookieParser());
+
 app.use("/api/posts" , postRoute);
 app.use("/api/auth" , authRoute);
 
--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -71,4 +71,5 @@ export const login=async(req , res)=>{
 
 export const logout=(req , res)=>{
     // db opeartions
+    res.clearCookie("token").status(200).json({message:"Logout Sucessfully"});
 }
