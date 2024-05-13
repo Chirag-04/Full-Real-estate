c247d871b58b50f721921c07514b1e5a7398b99b
Chirag-04 <95089015+Chirag-04@users.noreply.github.com>
2024-05-13 22:16:42 +0530

    started working on post feature

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,10 +1,14 @@
 import express from "express"
-const router =  express.Router()
+import {verifyToken} from "../middleware/verifyToken.js"
+import { addPost, deletePost, getPost, getPosts, updatePost } from "../controllers/post.controller.js";
 
-router.get( "/test" , (req,res)=>{
-    console.log("router works!!")
-})
+const router =  express.Router();
 
+router.get("/"  , getPosts);
+router.get("/:id" , getPost);
+router.post("/"  ,verifyToken ,  addPost);
+router.put("/:id"  ,verifyToken ,  updatePost);
+router.delete("/:id"  ,verifyToken ,  deletePost);
 
 
 export default router;
