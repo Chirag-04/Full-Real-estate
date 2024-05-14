10ee47bebf80d62352a8def9eccff8efe9f28247
Chirag-04 <95089015+Chirag-04@users.noreply.github.com>
2024-05-14 22:44:55 +0530

    made some changes

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -3,10 +3,26 @@ import prisma from '../lib/prisma.js'
 
 // get users
 export const getPosts = async(req , res) =>{
-    try{
-        const posts = await prisma.post.findMany();
-        res.status(200).json(posts); 
-    }catch(err){
+    const query = req.query;
+    try {
+        const posts = await prisma.post.findMany({
+          where: {
+            city: query.city || undefined,
+            type: query.type || undefined,
+            property: query.property || undefined,
+            bedroom: parseInt(query.bedroom) || undefined,
+            price: {
+              gte: parseInt(query.minPrice) || undefined,
+              lte: parseInt(query.maxPrice) || undefined,
+            },
+          },
+        });
+    
+        setTimeout(() => {
+
+        res.status(200).json(posts);
+        }, 2000);
+      } catch(err){
         console.log(err)
         res.status(500).json({message : "Failed to get posts"});
     }
